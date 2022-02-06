805966e8873f10031889f55e77f2c29148a139ac
Guilherme Barros <Guilhermescarmagnani@outlook.com>
2022-02-06 14:51:22 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    //Menu Hamburg
+    var $hamburger = $(".hamburger");
+    $hamburger.on("click", function(e) {
+      $hamburger.toggleClass("is-active");
+      $('nav#navMobile').toggle('dflex')
+    });    
+
+    //Owl Carousel
+    $('.owl-carousel').owlCarousel({
+        center: false,
+        items: 4,
+        loop: false,
+        margin:10,
+        autoWidth: true,        
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
+    $('.customNextBtn').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.customPrevBtn').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+});
